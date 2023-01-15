9353568b024ddaf22d85a3e6de3f69c9d95507a7
Emilio <hello@emicastro.com>
2023-01-15 17:32:50 -0300

    Initial configuration
    Basic config to make nvim my IDE.

---
+++ after/plugin/lualine.lua
@@ -0,0 +1,10 @@
+-- Set lualine as statusline
+-- See `:help lualine.txt`
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'powerline',
+    --component_separators = '|',
+    --section_separators = '',
+  },
+}
---
+++ init.lua
@@ -0,0 +1,1 @@
+require("emicastro")
