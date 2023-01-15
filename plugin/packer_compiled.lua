2eac29d3d66f0e7873fe4f410b119b5d25a4ac3d
Emilio <hello@emicastro.com>
2023-01-15 18:43:14 -0300

    Adds vim-be-good plugin
    It allows me to play games to accelerate my moves with vim.

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -205,6 +205,11 @@ _G.packer_plugins = {
     path = "/Users/emi/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
   },
+  ["vim-be-good"] = {
+    loaded = true,
+    path = "/Users/emi/.local/share/nvim/site/pack/packer/start/vim-be-good",
+    url = "https://github.com/theprimeagen/vim-be-good"
+  },
   ["vim-fugitive"] = {
     loaded = true,
     path = "/Users/emi/.local/share/nvim/site/pack/packer/start/vim-fugitive",
