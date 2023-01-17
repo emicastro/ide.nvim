ad38267a8718fd3094dbe865c423a3f04fd7a32c
Emilio <hello@emicastro.com>
2023-01-17 17:15:40 -0300

    Removes duplicated remap

--- lua/emicastro/remap.lua
+++ lua/emicastro/remap.lua
@@ -1,7 +1,6 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
---vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
 vim.keymap.set("n", "<leader>u", ":UndotreeShow<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
