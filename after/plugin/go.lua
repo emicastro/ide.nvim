f873e0007d1f45fa46c4d6089f412c26e679149d
Emilio <hello@emicastro.com>
2023-01-17 17:54:43 -0300

    Adds mapping for golang
    Adds '<leader>gg' to alternate between a file.go and a file_test.go

--- after/plugin/go.lua
+++ after/plugin/go.lua
@@ -9,3 +9,18 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
   group = format_sync_grp,
 })
+
+local autocmd = vim.api.nvim_create_autocmd
+autocmd("BufWinEnter", {
+    group = Emicastro_golang,
+    pattern = "*.go",
+    callback = function()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = {buffer = bufnr, remap = false}
+        -- alternate between file and file_test
+        vim.keymap.set("n", "<leader>gg", function()
+            vim.cmd [[ GoAlt ]]
+        end, opts)
+
+    end,
+})
