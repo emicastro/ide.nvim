a3241ca41d62efa1622ae843daed7c188b04a93e
Emilio <hello@emicastro.com>
2023-09-09 20:29:55 -0300

    Use <CR> to confirm lsp suggestions

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -38,7 +38,7 @@ local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
+	['<CR>'] = cmp.mapping.confirm({ select = true }),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
 	['<C-Space>'] = cmp.mapping.complete(),
