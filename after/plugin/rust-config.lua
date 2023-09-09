9c7d722d0fa614e32b2941af376b02232d378202
Emilio <hello@emicastro.com>
2023-09-09 16:18:02 -0300

    Adds Rust support with rust-tools

--- after/plugin/rust-config.lua
+++ after/plugin/rust-config.lua
@@ -13,9 +13,19 @@ rt.setup({
   server = {
     capabilities = require("cmp_nvim_lsp").default_capabilities(),
     on_attach = function(_, bufnr)
-      vim.keymap.set("n", "<Leader>k", rt.hover_actions.hover_actions, { buffer = bufnr })
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+      vim.keymap.set("n", "<Leader>h", rt.hover_actions.hover_actions, { buffer = bufnr })
+      vim.keymap.set("n", "<Leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
     end,
+    settings = {
+      -- to enable rust-analyzer settings visit:
+      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+      ["rust-analyzer"] = {
+        -- enable clippy on save
+        checkOnSave = {
+          command = "clippy",
+        },
+      },
+    },
   },
   tools = {
     hover_actions = {
