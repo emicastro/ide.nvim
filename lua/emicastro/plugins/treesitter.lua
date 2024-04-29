af46add2be23336992b80e59bb47988feb295556
Emilio <hello@emicastro.com>
2024-04-29 16:58:44 -0300

    Adds awesome keymaps to use treesitter-textobjects for blazzingly fast navigation

--- lua/emicastro/plugins/treesitter.lua
+++ lua/emicastro/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return { -- Highlight, edit, and navigate code
   build = ":TSUpdate",
   dependencies = {
     "windwp/nvim-ts-autotag",
+    "nvim-treesitter/nvim-treesitter-textobjects",
   },
   config = function()
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
@@ -64,46 +65,10 @@ return { -- Highlight, edit, and navigate code
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "<c-space>",
-          node_incremental = "<c-space>",
-          scope_incremental = "<c-s>",
-          node_decremental = "<c-backspace>",
-        },
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ["aa"] = "@parameter.outer",
-            ["ia"] = "@parameter.inner",
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            ["]m"] = "@function.outer",
-            ["]]"] = "@class.outer",
-          },
-          goto_next_end = {
-            ["]M"] = "@function.outer",
-            ["]["] = "@class.outer",
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ["<leader>a"] = "@parameter.inner",
-          },
-          swap_previous = {
-            ["<leader>A"] = "@parameter.inner",
-          },
+          init_selection = "<C-y>",
+          node_incremental = "<C-y>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
         },
       },
     })
