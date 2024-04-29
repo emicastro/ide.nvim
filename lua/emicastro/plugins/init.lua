67aba4e3977d211d0655701513f7c30a0cb775fe
Emilio <hello@emicastro.com>
2024-04-29 09:14:22 -0300

    Cellular-automaton and notes on treesitter/playground

--- lua/emicastro/plugins/init.lua
+++ lua/emicastro/plugins/init.lua
@@ -2,16 +2,21 @@
 return {
   "nvim-lua/plenary.nvim",
   "mbbill/undotree",
-
-  -- 'eandrju/cellular-automaton.nvim',
-  --
-  -- 'nvim-treesitter/playground',
-
   "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+
+  "eandrju/cellular-automaton.nvim",
+  -- The joy of vim motions by theprimeagen
+  "theprimeagen/vim-be-good",
 
   -- Use at your own risk
   --  'github/copilot.nvim',
 
-  -- The joy of vim motions by theprimeagen
-  "theprimeagen/vim-be-good",
+  -- AST visualizer in neovim
+  -- NOTE: This plugin is deactivated since the functionality
+  -- is included in Neovim: Use
+  -- :Inspect to show the highlight groups under the cursor
+  -- :InspectTree to show the parsed syntax tree ("TSPlayground")
+  -- :EditQuery to open the Live Query Editor (Nvim 0.10+)
+  --
+  -- 'nvim-treesitter/playground',
 }
