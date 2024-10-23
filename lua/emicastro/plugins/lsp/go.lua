fbaa57fa11b1ee3b23588f802858cfe9c9ddcd98
Emilio <hello@emicastro.com>
2024-10-23 20:03:14 -0300

    Adds keymaps for tests with go

--- lua/emicastro/plugins/lsp/go.lua
+++ lua/emicastro/plugins/lsp/go.lua
@@ -5,10 +5,26 @@ autocmd("BufWinEnter", {
   pattern = "*.go",
   callback = function()
     local bufnr = vim.api.nvim_get_current_buf()
-    -- alternate between file and file_test
+
     vim.keymap.set("n", "<leader>gg", function()
       vim.cmd([[ GoAlt ]])
     end, { buffer = bufnr, remap = false, desc = "alternate between file and file_test" })
+
+    vim.keymap.set("n", "<leader>gaf", function()
+      vim.cmd([[ GoAddTest]])
+    end, { buffer = bufnr, remap = false, desc = "add test to function" })
+
+    vim.keymap.set("n", "<leader>gtt", function()
+      vim.cmd([[ GoTestFunc ]])
+    end, { buffer = bufnr, remap = false, desc = "run function's test" })
+
+    vim.keymap.set("n", "<leader>gtf", function()
+      vim.cmd([[ GoTestFile ]])
+    end, { buffer = bufnr, remap = false, desc = "run all tests in file" })
+
+    vim.keymap.set("n", "<CR>", function()
+      vim.cmd([[ GoTest -n ]])
+    end, { buffer = bufnr, remap = false, desc = "run nearest test" })
 
     vim.keymap.set("n", "<leader>gie", function()
       vim.cmd([[ GoIfErr ]])
