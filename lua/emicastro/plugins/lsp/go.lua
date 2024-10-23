c4b6502fc0416cb94f8d7a27a0676ba07146fefd
Emilio <hello@emicastro.com>
2024-10-23 19:37:45 -0300

    Adds keymaps for .go files

--- lua/emicastro/plugins/lsp/go.lua
+++ lua/emicastro/plugins/lsp/go.lua
@@ -1,3 +1,25 @@
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("BufWinEnter", {
+  group = Emicastro_golang,
+  pattern = "*.go",
+  callback = function()
+    local bufnr = vim.api.nvim_get_current_buf()
+    -- alternate between file and file_test
+    vim.keymap.set("n", "<leader>gg", function()
+      vim.cmd([[ GoAlt ]])
+    end, { buffer = bufnr, remap = false, desc = "alternate between file and file_test" })
+
+    vim.keymap.set("n", "<leader>gie", function()
+      vim.cmd([[ GoIfErr ]])
+    end, { buffer = bufnr, remap = false, desc = "generate if error block" })
+
+    vim.keymap.set("n", "<leader>gfs", function()
+      vim.cmd([[ GoFillStruct ]])
+    end, { buffer = bufnr, remap = false, desc = "fill struct" })
+  end,
+})
+
 return {
   "ray-x/go.nvim",
   dependencies = { -- optional packages
@@ -12,36 +34,3 @@ return {
   ft = { "go", "gomod" },
   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 }
-
---[[local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-   require('go.format').goimport()
-  end,
-  group = format_sync_grp,
-})
-
-local autocmd = vim.api.nvim_create_autocmd
-autocmd("BufWinEnter", {
-    group = Emicastro_golang,
-    pattern = "*.go",
-    callback = function()
-        local bufnr = vim.api.nvim_get_current_buf()
-        local opts = {buffer = bufnr, remap = false}
-        -- alternate between file and file_test
-        vim.keymap.set("n", "<leader>gg", function()
-            vim.cmd  GoAlt 
-        end, opts)
-
-        vim.keymap.set("n", "<leader>gie", function()
-            vim.cmd [[ GoIfErr ]
-        end, opts)
-
-        vim.keymap.set("n", "<leader>gfs", function()
-            vim.cmd [[ GoFillStruct ]
-        end, opts)
-
-    end,
-})
-]]
